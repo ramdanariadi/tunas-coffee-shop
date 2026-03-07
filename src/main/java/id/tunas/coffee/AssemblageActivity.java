ed9ef34177eff60b3d92aebc5f41055e6396436d
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-03-07 12:29:14 +0800

    feat: implement barista selection in AssemblageActivity with ActivityResultLauncher

--- src/main/java/id/tunas/coffee/AssemblageActivity.java
+++ src/main/java/id/tunas/coffee/AssemblageActivity.java
@@ -1,11 +1,18 @@
 package id.tunas.coffee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AssemblageActivity extends AppCompatActivity {
+
+    private String baristaName;
+    private String baristaId;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -13,5 +20,20 @@ public class AssemblageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_assemblage);
 
         findViewById(R.id.arrow_left).setOnClickListener(view -> finish());
+
+        ActivityResultLauncher<Intent> selectBaristaLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    this.baristaId = result.getData().getStringExtra("barista_id");
+                    this.baristaName = result.getData().getStringExtra("barista_name");
+                    TextView selectBaristaTitle = findViewById(R.id.select_barista_title);
+                    StringBuilder sb = new StringBuilder(getString(R.string.select_barista));
+                    sb.append(" - ").append(this.baristaName);
+                    selectBaristaTitle.setText(sb.toString());
+                }
+        );
+
+        Intent intent = new Intent(this, SelectBaristaActivity.class);
+        selectBaristaLauncher.launch(intent);
     }
 }
