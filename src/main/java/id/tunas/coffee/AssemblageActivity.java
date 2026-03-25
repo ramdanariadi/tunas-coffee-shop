740fd1d5cc3dd3d694e70364b9be819adb97cf7c
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-03-25 15:47:01 +0800

    feat: enhance onboarding experience with slider navigation and page indicator updates

--- src/main/java/id/tunas/coffee/AssemblageActivity.java
+++ src/main/java/id/tunas/coffee/AssemblageActivity.java
@@ -2,14 +2,16 @@ package id.tunas.coffee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
@@ -17,11 +19,13 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.common.base.Strings;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
+import java.util.logging.Logger;
+
 import id.tunas.coffee.adapter.OnBoardingAdapter;
 import id.tunas.coffee.dto.OnBoardingItem;
 
 public class AssemblageActivity extends AppCompatActivity {
-
+    private static final Logger LOGGER = Logger.getLogger(AssemblageActivity.class.getName());
     private String baristaName;
     private String baristaId;
 
@@ -35,6 +39,7 @@ public class AssemblageActivity extends AppCompatActivity {
         ActivityResultLauncher<Intent> selectBaristaLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
+                    if(result.getResultCode() != RESULT_OK) return;
                     String baristaId = result.getData().getStringExtra("barista_id");
                     String baristaName = result.getData().getStringExtra("barista_name");
                     if(!Strings.isNullOrEmpty(baristaId) && !Strings.isNullOrEmpty(baristaName)){
@@ -58,18 +63,58 @@ public class AssemblageActivity extends AppCompatActivity {
         bottomSheetDialog.setContentView(v);
 
         v.findViewById(R.id.cancel_select_milk).setOnClickListener(view -> bottomSheetDialog.dismiss());
-
-        findViewById(R.id.select_milk).setOnClickListener(view -> {
-            bottomSheetDialog.show();
-        });
+        findViewById(R.id.select_milk).setOnClickListener(view -> bottomSheetDialog.show());
 
         OnBoardingAdapter onBoardingAdapter = new OnBoardingAdapter(OnBoardingItem.createDummy());
         ViewPager2 viewPager = findViewById(R.id.viewPager);
         TabLayout tabIndicator = findViewById(R.id.tabIndicator);
         viewPager.setAdapter(onBoardingAdapter);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                for(int i = 0; i < tabIndicator.getTabCount(); i++){
+                    TabLayout.Tab tab = tabIndicator.getTabAt(i);
+                    if(tab != null && tab.getCustomView() != null){
+                        View customView = tab.getCustomView();
+                        View dot = customView.findViewById(R.id.dot);
+                        if(i == position){
+                            dot.setAlpha(1.0F);
+                        }else{
+                            dot.setAlpha(0.2F);
+                        }
+                    }
+                }
+            }
+        });
         TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabIndicator, viewPager, (tab, i) -> {
+            tab.setCustomView(R.layout.custom_dot);
         });
         tabLayoutMediator.attach();
+        viewPager.setCurrentItem(0, false);
+
+        TextView previousSlider = findViewById(R.id.previous_slider);
+        TextView nextSlider = findViewById(R.id.next_slider);
+        LinearLayout sliderLayout = findViewById(R.id.slider);
+        previousSlider.setOnClickListener(view -> {
+            if(viewPager.getCurrentItem() == 0) return;
+            if(viewPager.getCurrentItem() == 1){
+                previousSlider.setTextColor(getColor(R.color.grey_border));
+            };
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1, true);
+        });
+
+        nextSlider.setOnClickListener(view -> {
+            if(viewPager.getCurrentItem() == onBoardingAdapter.getItemCount() - 1){
+                sliderLayout.setVisibility(View.GONE);
+                return;
+            }
+            if(viewPager.getCurrentItem() == (onBoardingAdapter.getItemCount() - 2)){
+                nextSlider.setText("Done");
+            }
+            previousSlider.setTextColor(getColor(R.color.card_white));
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+        });
 
     }
 }
